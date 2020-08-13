e86b9d9c25de064cb292319c12f8afb2020d55bd
Harry Pierson <harrypierson@ngd.neo.org>
2020-08-13 14:36:54 -0700

    update ci build

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,5 @@ const gulp = require('gulp');
 const nbgv = require('nerdbank-gitversioning')
 
 gulp.task('setversion', function() {
-    nbgv.resetPackageVersionPlaceholder();
     return nbgv.setPackageVersion();
 });
